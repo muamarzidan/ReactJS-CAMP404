8a9f52800ca0bb73255ffd3c3cc834377e264047
Muamar Zidan Tri Antoro <89593258+muamarzidan@users.noreply.github.com>
2023-06-19 23:43:03 +0700

    logic tabel

--- src/components/ManejemenBuku.jsx
+++ src/components/ManejemenBuku.jsx
@@ -1,28 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TabelBuku from './TabelBuku';
 
 function ManejemenBuku () {
+    const [formMode, setFormMode] = useState("");
+
+    function showcreateFrom() {
+        setFormMode("show");
+    }
+
     return (
         <div className="container mt-3">
             <h1 className="text-center">Manejemen Buku</h1>
-            <button className='btn btn-sm btn-primary' >Tambah Buku</button>
-
-            <div id='form' className='card py-2 my-3 bg-secondary'>
-                <div className='card-body'>
-                    <h5 className='card-title text-white'>Form</h5>
-                    <form className='row'>
-                        <div className='col-6'>
-                            <input type='text' name='judul' className='form-control mx-2' placeholder='judul' />
-                        </div>
-                        <div className='col-4'>
-                            <input type='text' name='pengarang' className='form-control mx-2' placeholder='judul' />
-                        </div>
-                        <div className='col-2'>
-                            <button className='btn btn-primary'>Submit</button>
-                        </div>
-                    </form>
+            <button className='btn btn-sm btn-primary' onClick={showcreateFrom} >Tambah Buku</button>
+            {formMode === "show" && (
+                <div id='form' className='card py-2 my-3 bg-secondary'>
+                    <div className='card-body'>
+                        <h5 className='card-title text-white'>Form</h5>
+                        <form className='row'>
+                            <div className='col-6'>
+                                <input type='text' name='judul' className='form-control mx-2' placeholder='judul' />
+                            </div>
+                            <div className='col-4'>
+                                <input type='text' name='pengarang' className='form-control mx-2' placeholder='judul' />
+                            </div>
+                            <div className='col-2'>
+                                <button className='btn btn-primary'>Submit</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-            </div>
+            )}
             <TabelBuku />
         </div>
     );
