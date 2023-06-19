a089b3fb3ef36ddf9ae301c565ba56182d8585e9
Muamar Zidan Tri Antoro <89593258+muamarzidan@users.noreply.github.com>
2023-06-19 23:15:17 +0700

    new form

--- src/components/Beranda.jsx
+++ src/components/Beranda.jsx
@@ -2,8 +2,26 @@ import React from 'react';
 
 function Beranda () {
     return (
-        <div className="container mt-3 w-75">
+        <div className="container mt-3">
             <h1 className="text-center">Beranda</h1>
+            <button className='btn btn-sm btn-primary my-2'>SINI</button>
+
+            <div id='form' className='card py-2 my-3 bg-secondary'>
+                <div className='card-body'>
+                    <h5 className='card-title text-white'>Form</h5>
+                    <form className='row'>
+                        <div className='col-6'>
+                            <input type='text' name='judul' className='form-control mx-2' placeholder='judul' />
+                        </div>
+                        <div className='col-4'>
+                            <input type='text' name='pengarang' className='form-control mx-2' placeholder='judul' />
+                        </div>
+                        <div className='col-2'>
+                            <button className='btn btn-primary'>Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 }
